7c919f82db146e895427de00db82ca157d52bc79
luckschavalo <lucaschl1007@gmail.com>
2025-02-05 15:39:12 -0600

    tarea de html
    Primera tarea usando html

---
+++ PracticaHTML001/src/main/java/Practica01Lucas/Practica01Lucas/Practica01LucasApplication.java
@@ -0,0 +1,13 @@
+package Practica01Lucas.Practica01Lucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica01LucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica01LucasApplication.class, args);
+	}
+
+}
---
+++ PracticaHTML01/src/test/java/Practica01Lucas/Practica01Lucas/Practica01LucasApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica01Lucas.Practica01Lucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica01LucasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
